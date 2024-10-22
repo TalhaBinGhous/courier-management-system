5c5bfbfa3ae4f25cc0b1715d606c787a92edc58e
Talha <talhabinghous@aptechnorth.edu.pk>
2024-10-22 19:04:36 +0500

    first commit

---
+++ admin/conn.php
@@ -0,0 +1,3 @@
+<?php
+$conn = mysqli_connect('localhost','root','','trek_track');
+?>
---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pw = "";
+$db = "trek_track";
+
+$conn = mysqli_connect($host,$user,$pw,$db);
+
+?>
